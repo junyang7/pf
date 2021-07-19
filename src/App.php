b16c54689de33f2f864727dc599f6673a2e7bbe7
阳仔仔 <507979696@qq.com>
2021-07-19 22:14:55 +0800

    入口

---
+++ src/App.php
@@ -0,0 +1,27 @@
+<?php
+
+
+namespace Pf;
+
+
+class App
+{
+
+
+    public function __construct($base_dir)
+    {
+
+        echo $base_dir . PHP_EOL;
+
+    }
+
+
+    public function run()
+    {
+
+        echo 'Hello Pf!';
+
+    }
+
+
+}

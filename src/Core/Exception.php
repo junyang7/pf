af2abb00154c0b313f381437064dcfb3a26e3df4
guoguo <507979696@qq.com>
2021-07-22 18:59:13 +0800

    调整：异常响应

--- src/Core/Exception.php
+++ src/Core/Exception.php
@@ -19,20 +19,7 @@ class Exception
     public static function handleException($exception)
     {
 
-        if(get_class($exception) == 'Pf\Core\PfException')
-        {
-            echo 'code: ' . $exception->getPfCode() . PHP_EOL;
-            echo 'info: ' . $exception->getPfInfo() . PHP_EOL;
-            echo 'data: ' . json_encode($exception->getPfData(), JSON_UNESCAPED_UNICODE) . PHP_EOL;
-        }
-        else
-        {
-            echo 'code: ' . $exception->getCode() . PHP_EOL;
-            echo 'info: ' . $exception->getMessage() . PHP_EOL;
-            echo 'data: ' . json_encode($exception->getTrace(), JSON_UNESCAPED_UNICODE) . PHP_EOL;
-        }
-        echo 'file: ' . $exception->getFile() . PHP_EOL;
-        echo 'line: ' . $exception->getLine() . PHP_EOL;
+        Render::exception($exception);
 
     }
 
--- src/Core/Render.php
+++ src/Core/Render.php
@@ -33,4 +33,61 @@ class Render
     }
 
 
+    public static function exception($exception)
+    {
+
+        $app = \Pf\App::getInstance();
+        $app->response = [];
+
+        if(get_class($exception) == 'Pf\Core\PfException')
+        {
+
+            $app->response['code'] = $exception->getPfCode();
+            $app->response['info'] = $exception->getPfInfo();
+            $app->response['data'] = $exception->getPfData();
+
+        }
+        else
+        {
+
+            $app->response['code'] = -1;
+            $app->response['info'] = $exception->getMessage();
+            $app->response['data'] = $exception->getTrace();
+
+        }
+
+        if(defined('APP_DEBUG') && constant('APP_DEBUG'))
+        {
+
+            $app->response['file'] = $exception->getFile();
+            $app->response['line'] = $exception->getLine();
+
+        }
+        else
+        {
+
+            if(get_class($exception) != 'Pf\Core\PfException')
+            {
+
+                $app->response['info'] = '系统繁忙，稍后再试';
+                $app->response['data'] = NULL;
+
+            }
+
+        }
+
+        switch($app->request->env)
+        {
+            case $app->env_api:
+                header('Content-Type: application/json');
+                echo json_encode($app->response);
+                break;
+            default:
+                echo $app->response['info'];
+                break;
+        }
+
+    }
+
+
 }
